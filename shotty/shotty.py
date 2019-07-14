9a8fb124309bb7ec016ba17a578695fa8de3cc5d
kazu <kazu@kazuhiro-no-iMac-3.local>
2019-07-15 07:55:27 +0900

    update script

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,8 +1,20 @@
 import boto3
+import sys
+
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
+
+@click.command()
+def list_instances():
+    "list EC2 instances"
+    for i in ec2.instances.all():
+        print(',''.join((
+        i.# IDEA: i.instance_type,
+        i.placement['availablityzone'],
+        i.state['name'],
+        i.public_dns_name)))
+
+    return
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name='shotty')
-    ec2 = session.resource('ec2')
-    
-    for i in ec2.instances.all():
-        print(i)
+    list_instances()
